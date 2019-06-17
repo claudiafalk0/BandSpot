d3183262dfed4c4fbe98a51515d6b100cec56d9e
Andrew Ulrich <andyinthemachine@hotmail.com>
2019-06-17 14:49:21 -0600

    choose album

--- assets/javascript/code.js
+++ assets/javascript/code.js
@@ -4,41 +4,102 @@
 // Deedzer api key 6f4d1eb22866cf66982fcd2dcbcdce2b
 
 
-
-console.log("in js");
-
-var band = "The Beatles";
 var api_key = "6f4d1eb22866cf66982fcd2dcbcdce2b"
 var back_end_proxy = "https://cors-anywhere.herokuapp.com/";
-var queryURL = back_end_proxy + "https://api.deezer.com/search/?q=" + band + "&api_key=" + api_key;
+var album_id = "";
 
 
-  $.get({
-    url: queryURL,
-  }).then(function(response) {
-      console.log(response);
-      var mydata  = response.data;
+// var band = "Chet Baker";
+//var queryURL_Artist = back_end_proxy + "https://api.deezer.com/search/?q=" + band + "&api_key=" + api_key;
 
-      console.log(mydata[0].title); 
+// $.get({ url: queryURL_Artist, }).then(function (response) {
+//   console.log(response);
+//   var mydata = response.data;
 
-      for(i = 0; i < mydata.length; i++) {
-          console.log(mydata[i].title + "/n");
-          $("#songs").append(mydata[i].title, "<br>");
-}
-    //   mydata.forEach(function(item, i) {
-    //       console.log(item[i]);
-    //       $("#songs").append(item[i] + "/n");
-    //   });
+//   console.log(mydata[0].title);
+
+//   mydata.forEach(function (item) {
+//     console.log(item.title);
+//     $("#band-info").append(item.title + "<br>");
+//   });
+// });
+
+//https://api.deezer.com/search?q=b&api_key=6f4d1eb22866cf66982fcd2dcbcdce2b
+//https://api.deezer.com/search?q=album:"good things"
+
+
+$("#album_search_text").on("keyup", function () {
+  $("#album_search_dropdown").empty();
 });
-    // Methods run on jQuery selectors return the selector they we run on
-    // This is why we can create and save a reference to a td in the same statement we update its text
-    // var titleTd = $("<td>").text(response.Title);
-    // var yearTd = $("<td>").text(response.Year);
-    // var actorsTd = $("<td>").text(response.Actors);
-      
-    // // Append the newly created table data to the table row
-    // tRow.append(titleTd, yearTd, actorsTd);
-    // Append the table row to the table body
-    // $("tbody").append(tRow);
 
-  // https://api.deezer.com/search/?q=beatles&api_key=6f4d1eb22866cf66982fcd2dcbcdce2b
+$(".input-group-append").on("click", ".dropdown-item", function () {
+
+  console.log($(this).attr("data-album-id"));
+  album_id = $(this).attr("data-album-id");
+
+  show_album();
+});
+
+$("#album_search_button").on("click", function () {
+
+  var album_search_str = $("#album_search_text").val().trim();
+  var queryURL_Album_Search = back_end_proxy + "https://api.deezer.com/search?q=album:" + album_search_str + "&api_key=" + api_key;
+
+  $.get({ url: queryURL_Album_Search, }).then(function (response) {
+
+    var mydata = response.data;
+    mydata.forEach(function (item) {
+      var new_list_item = $("<div>");
+      new_list_item.addClass("dropdown-item");
+      new_list_item.attr("data-album-id", item.album.id);
+      new_list_item.text(item.album.title);
+      $("#album_search_dropdown").append(new_list_item);
+    });
+
+  });
+});
+
+
+function show_album() {
+
+  var queryURL_Album = back_end_proxy + "https://api.deezer.com/album/" + album_id + "&api_key=" + api_key;
+
+  $("#band-info").empty();
+
+  $.get({ url: queryURL_Album, }).then(function (response) {
+
+    $("#band-info").append("<br>" + response.artist.name + " - ");
+    $("#band-info").append(response.title + "<br><br>");
+
+    var album_cover = $("<img>");
+    album_cover.attr("src", response.cover_medium);
+    $("#band-info").append(album_cover);
+    $("#band-info").append("<br><br>");
+
+    // var album_tracks = response.tracks.data;
+    // album_tracks.forEach(function (item) {
+    //   console.log(item.title);
+    //   $("#band-info").append(item.title + "<br>");
+    // });
+  });
+
+  var widget_album = "https://www.deezer.com/plugins/player?format=classic&autoplay=false&playlist=true&width=700&height=350&color=ff0000&layout=dark&size=medium&type=album&id=" + album_id + "&app_id=353884";
+  $(".deezer-widget-player").attr("data-src", widget_album);
+
+  (function () {
+    var w = document[typeof document.getElementsByClassName === 'function' ? 'getElementsByClassName' : 'querySelectorAll']('deezer-widget-player');
+    for (var i = 0, l = w.length; i < l; i++) {
+      w[i].innerHTML = '';
+      var el = document.createElement('iframe');
+      el.src = w[i].getAttribute('data-src');
+      el.scrolling = w[i].getAttribute('data-scrolling');
+      el.frameBorder = w[i].getAttribute('data-frameborder');
+      el.setAttribute('frameBorder', w[i].getAttribute('data-frameborder'));
+      el.allowTransparency = w[i].getAttribute('data-allowTransparency');
+      el.width = w[i].getAttribute('data-width');
+      el.height = w[i].getAttribute('data-height');
+      w[i].appendChild(el);
+    }
+  }());
+
+}
