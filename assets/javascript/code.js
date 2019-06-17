e2e62347969a945ebe24ea4cce17296776dcf7e5
Christian Pak <iamchristianpak@gmail.com>
2019-06-17 13:29:26 -0600

    styling and last.fm api

---
+++ assets/javascript/code.js
@@ -0,0 +1,25 @@
+
+$("#submitBtn").on("click", function () {
+    var artist = "eminem"
+    var api_key = "4449581c4e4db7c380fae2d8fd50142d"
+    var method = "artist.getinfo"
+    
+    // Constructing a queryURL using the animal name
+    var queryURL = "http://ws.audioscrobbler.com/2.0/?method=" + method + "&artist=" + artist + "&api_key=" + api_key + "&format=json";
+
+    // Performing an AJAX request with the queryURL
+    $.ajax({
+            url: queryURL,
+            method: "GET"
+        })
+        // After data comes back from the request
+        .then(function (response) {
+            console.log(queryURL);
+
+            console.log(response);
+            // storing the data from the AJAX request in the results variable
+            var results = response.artist.bio.summary;
+            console.log(results)
+            $("#bio").text(results)
+        });
+});
