918f6b9bc675bac951d2d876ec9684c2f77e31a7
Christian Pak <iamchristianpak@gmail.com>
2019-06-17 13:53:50 -0600

    removed ' <' from summary, styled, tryied to be responsive, didnt work

--- assets/javascript/code.js
+++ assets/javascript/code.js
@@ -1,11 +1,10 @@
-
 $("#submitBtn").on("click", function () {
-    var artist = "eminem"
+    var artist = "DJ khalid"
     var api_key = "4449581c4e4db7c380fae2d8fd50142d"
     var method = "artist.getinfo"
-    
+
     // Constructing a queryURL using the animal name
-    var queryURL = "http://ws.audioscrobbler.com/2.0/?method=" + method + "&artist=" + artist + "&api_key=" + api_key + "&format=json";
+    var queryURL = "http://ws.audioscrobbler.com/2.0/?method=" + method + "&artist=" + artist + "&api_key=" + api_key + "&format=json&autocorrect=1";
 
     // Performing an AJAX request with the queryURL
     $.ajax({
@@ -19,7 +18,9 @@ $("#submitBtn").on("click", function () {
             console.log(response);
             // storing the data from the AJAX request in the results variable
             var results = response.artist.bio.summary;
-            console.log(results)
-            $("#bio").text(results)
+
+            var values = results.split(" <").shift();
+
+            $("#bio").text(values)
         });
 });
