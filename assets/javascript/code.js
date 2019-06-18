59101febadd6a783735fc912aeea384e895778c8
Christian Pak <iamchristianpak@gmail.com>
2019-06-18 09:47:32 -0600

    finished layout

--- assets/javascript/code.js
+++ assets/javascript/code.js
@@ -2,106 +2,102 @@
 var api_key = "6f4d1eb22866cf66982fcd2dcbcdce2b"
 var back_end_proxy = "https://cors-anywhere.herokuapp.com/";
 var album_id = "";
+var artistBio = "";
+var summary_api_key = "4449581c4e4db7c380fae2d8fd50142d"
+var summary_method = "artist.getinfo"
+var summaryURL = "http://ws.audioscrobbler.com/2.0/?method=" + summary_method + "&api_key=" + summary_api_key + "&format=json&autocorrect=1";
 
 $("#album_search_text").on("keyup", function () {
-  $("#album_search_dropdown").empty();
+    $("#album_search_dropdown").empty();
 });
 
 $(".input-group-append").on("click", ".dropdown-item", function () {
-  console.log($(this).attr("data-album-id"));
-  album_id = $(this).attr("data-album-id");
-  $("#album_search_dropdown").empty();
-  show_album();
+    console.log($(this).attr("data-album-id"));
+    album_id = $(this).attr("data-album-id");
+    $("#album_search_dropdown").empty();
+    show_album();
 });
 
 $("#album_search_button").on("click", function () {
 
-  var album_search_str = $("#album_search_text").val().trim();
-  var queryURL_Album_Search = back_end_proxy + "https://api.deezer.com/search?q=album:" + album_search_str + "&api_key=" + api_key;
+    var album_search_str = $("#album_search_text").val().trim();
+    var queryURL_Album_Search = back_end_proxy + "https://api.deezer.com/search?q=album:" + album_search_str + "&api_key=" + api_key;
 
-  $.get({ url: queryURL_Album_Search, async: false,}).then(function (response) {
+    $.get({
+        url: queryURL_Album_Search,
+        async: false,
+    }).then(function (response) {
 
-    var mydata = response.data;
-    mydata.forEach(function (item) {
-      var new_list_item = $("<div>");
-      new_list_item.addClass("dropdown-item");
-      new_list_item.attr("data-album-id", item.album.id);
-      new_list_item.text(item.title);
-      $("#album_search_dropdown").append(new_list_item);
+        var mydata = response.data;
+        mydata.forEach(function (item) {
+            var new_list_item = $("<div>");
+            new_list_item.addClass("dropdown-item");
+            new_list_item.attr("data-album-id", item.album.id);
+            new_list_item.text(item.title);
+            $("#album_search_dropdown").append(new_list_item);
+        });
+
     });
-
-  });
-  $("#album_search_dropdown").empty();
+    $("#album_search_dropdown").empty();
 });
 
 
 function show_album() {
 
-  var queryURL_Album = back_end_proxy + "https://api.deezer.com/album/" + album_id + "&api_key=" + api_key;
+    var queryURL_Album = back_end_proxy + "https://api.deezer.com/album/" + album_id + "&api_key=" + api_key;
 
-  $.get({ url: queryURL_Album, async: false}).then(function (response) {
+    $.get({
+        url: queryURL_Album,
+    }).then(function (response) {
+        console.log(response)
+        var artistName = response.artist.name
+        $("#artistName").text(artistName)
 
-    $("#artistName").text(response.artist.name);
-    // $("#musicInfo").append(response.title);
-    console.log(response.artist.name)
+        var album_cover = $("<img>");
+        album_cover.attr("src", response.cover_medium);
+        $("#artistImage").html(album_cover);
 
-    var album_cover = $("<img>");
-    album_cover.attr("src", response.cover_medium);
-    $("#artistImage").html(album_cover);
+        artistBio += "&artist=" + artistName;
+        summaryURL += artistBio;
+        console.log(artistBio)
+        artist(summaryURL);
+    });
 
-    // var album_tracks = response.tracks.data;
-    // album_tracks.forEach(function (item) {
-    //   console.log(item.title);
-    //   $("#musicInfo").append(item.title + "<br>");
-    // });
-  });
+    var widget_album = "https://www.deezer.com/plugins/player?format=classic&autoplay=false&playlist=true&width=700&height=350&color=ff0000&layout=dark&size=medium&type=album&id=" + album_id + "&app_id=353884";
+    $(".deezer-widget-player").attr("data-src", widget_album);
 
-  var widget_album = "https://www.deezer.com/plugins/player?format=classic&autoplay=false&playlist=true&width=700&height=350&color=ff0000&layout=dark&size=medium&type=album&id=" + album_id + "&app_id=353884";
-  $(".deezer-widget-player").attr("data-src", widget_album);
-
-  (function () {
-    var w = document[typeof document.getElementsByClassName === 'function' ? 'getElementsByClassName' : 'querySelectorAll']('deezer-widget-player');
-    for (var i = 0, l = w.length; i < l; i++) {
-      w[i].innerHTML = '';
-      var el = document.createElement('iframe');
-      el.src = w[i].getAttribute('data-src');
-      el.scrolling = w[i].getAttribute('data-scrolling');
-      el.frameBorder = w[i].getAttribute('data-frameborder');
-      el.setAttribute('frameBorder', w[i].getAttribute('data-frameborder'));
-      el.allowTransparency = w[i].getAttribute('data-allowTransparency');
-      el.width = w[i].getAttribute('data-width');
-      el.height = w[i].getAttribute('data-height');
-      w[i].appendChild(el);
-    }
-  }());
-
+    (function () {
+        var w = document[typeof document.getElementsByClassName === 'function' ? 'getElementsByClassName' : 'querySelectorAll']('deezer-widget-player');
+        for (var i = 0, l = w.length; i < l; i++) {
+            w[i].innerHTML = '';
+            var el = document.createElement('iframe');
+            el.src = w[i].getAttribute('data-src');
+            el.scrolling = w[i].getAttribute('data-scrolling');
+            el.frameBorder = w[i].getAttribute('data-frameborder');
+            el.setAttribute('frameBorder', w[i].getAttribute('data-frameborder'));
+            el.allowTransparency = w[i].getAttribute('data-allowTransparency');
+            el.width = w[i].getAttribute('data-width');
+            el.height = w[i].getAttribute('data-height');
+            w[i].appendChild(el);
+        }
+    }());
 }
 
-$("#submitBtn").on("click", function () {
-    var artist = "DJ khalid"
-    var api_key = "4449581c4e4db7c380fae2d8fd50142d"
-    var method = "artist.getinfo"
-
-    // Constructing a queryURL using the animal name
-    var queryURL = "http://ws.audioscrobbler.com/2.0/?method=" + method + "&artist=" + artist + "&api_key=" + api_key + "&format=json&autocorrect=1";
-
+function artist() {
     // Performing an AJAX request with the queryURL
     $.ajax({
-            url: queryURL,
+            url: summaryURL,
             method: "GET",
-            async: false,
         })
         // After data comes back from the request
         .then(function (response) {
-            console.log(queryURL);
 
-            console.log(response);
+            console.log(response)
             // storing the data from the AJAX request in the results variable
-            var results = response.artist.bio.summary;
-
+            var results = (response.artist.bio.summary);
+            console.log(results)
             var values = results.split(" <").shift();
 
-            $("#bio").text(values)
+            $("#bio").html(values)
         });
-});
-
+}
