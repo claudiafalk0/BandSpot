fb37033a5c4fc5582edf390cb642724ee4b3ffdd
Andrew Ulrich <andyinthemachine@hotmail.com>
2019-06-17 19:15:40 -0600

    cleaned up repeating album listings

--- assets/javascript/code.js
+++ assets/javascript/code.js
@@ -1,6 +1,5 @@
 
 
-
 // Deedzer api key 6f4d1eb22866cf66982fcd2dcbcdce2b
 
 
@@ -9,31 +8,13 @@ var back_end_proxy = "https://cors-anywhere.herokuapp.com/";
 var album_id = "";
 
 
-// var band = "Chet Baker";
-//var queryURL_Artist = back_end_proxy + "https://api.deezer.com/search/?q=" + band + "&api_key=" + api_key;
-
-// $.get({ url: queryURL_Artist, }).then(function (response) {
-//   console.log(response);
-//   var mydata = response.data;
-
-//   console.log(mydata[0].title);
-
-//   mydata.forEach(function (item) {
-//     console.log(item.title);
-//     $("#band-info").append(item.title + "<br>");
-//   });
-// });
-
-//https://api.deezer.com/search?q=b&api_key=6f4d1eb22866cf66982fcd2dcbcdce2b
-//https://api.deezer.com/search?q=album:"good things"
-
-
 $("#album_search_text").on("keyup", function () {
+  event.preventDefault();
   $("#album_search_dropdown").empty();
 });
 
 $(".input-group-append").on("click", ".dropdown-item", function () {
-
+  event.preventDefault();
   console.log($(this).attr("data-album-id"));
   album_id = $(this).attr("data-album-id");
 
@@ -41,19 +22,28 @@ $(".input-group-append").on("click", ".dropdown-item", function () {
 });
 
 $("#album_search_button").on("click", function () {
-
+  event.preventDefault();
   var album_search_str = $("#album_search_text").val().trim();
   var queryURL_Album_Search = back_end_proxy + "https://api.deezer.com/search?q=album:" + album_search_str + "&api_key=" + api_key;
 
+  $("#album_search_text").val("");
   $.get({ url: queryURL_Album_Search, }).then(function (response) {
 
+    album_ids = [];
+    album_titles = [];
+    console.log(response);
     var mydata = response.data;
-    mydata.forEach(function (item) {
-      var new_list_item = $("<div>");
-      new_list_item.addClass("dropdown-item");
-      new_list_item.attr("data-album-id", item.album.id);
-      new_list_item.text(item.album.title);
-      $("#album_search_dropdown").append(new_list_item);
+    mydata.forEach(function (item, i) {
+      // need to pass on mulitple listings of same album
+      if ((album_ids.indexOf(item.album.id) == -1) && (album_titles.indexOf(item.album.title) == -1)){
+        album_ids.push(item.album.id);
+        album_titles.push(item.album.title);
+        var new_list_item = $("<div>");
+        new_list_item.addClass("dropdown-item");
+        new_list_item.attr("data-album-id", item.album.id);
+        new_list_item.text(item.artist.name + " : " + item.album.title);
+        $("#album_search_dropdown").append(new_list_item);
+      }
     });
 
   });
@@ -76,11 +66,7 @@ function show_album() {
     $("#band-info").append(album_cover);
     $("#band-info").append("<br><br>");
 
-    // var album_tracks = response.tracks.data;
-    // album_tracks.forEach(function (item) {
-    //   console.log(item.title);
-    //   $("#band-info").append(item.title + "<br>");
-    // });
+    
   });
 
   var widget_album = "https://www.deezer.com/plugins/player?format=classic&autoplay=false&playlist=true&width=700&height=350&color=ff0000&layout=dark&size=medium&type=album&id=" + album_id + "&app_id=353884";
@@ -103,3 +89,22 @@ function show_album() {
   }());
 
 }
+
+//var band = "Chet Baker";
+//var queryURL_Artist = back_end_proxy + "https://api.deezer.com/search/?q=" + band + "&api_key=" + api_key;
+
+// $.get({ url: queryURL_Artist, }).then(function (response) {
+//   console.log(response);
+//   var mydata = response.data;
+//   mydata.forEach(function (item) {
+//     console.log(item.title);
+//     $("#band-info").append(item.title + "<br>");
+//   });
+// });
+
+// var album_tracks = response.tracks.data;
+    // album_tracks.forEach(function (item) {
+    //   console.log(item.title);
+    //   $("#band-info").append(item.title + "<br>");
+    // });
+
